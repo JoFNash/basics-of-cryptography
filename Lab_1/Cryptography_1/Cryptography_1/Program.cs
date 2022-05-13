0b4c2ede5cca1435099e2d16e74da949ac6dd918
JoFNash <olga@LAPTOP-1H4CR6LI.localdomain>
2022-05-13 20:26:41 +0300

    smth wrong

--- Lab_1/Cryptography_1/Cryptography_1/Program.cs
+++ Lab_1/Cryptography_1/Cryptography_1/Program.cs
@@ -9,63 +9,42 @@ namespace Cryptography_1
 
         public static byte[] BitSwapping(byte[] value, int[] rule)
         {
-            int i = 1;
-            ulong value_num = BitConverter.ToUInt32(value, 0);
+            int i = 0;
+            ulong value_num = BitConverter.ToUInt64(value, 0);
             ulong res = 0;
-
-            if (value.Length != rule.Length)
-            {
-                throw new ArgumentException("Incorrect value argument");
-            }
             
-            // Console.WriteLine("k = " + rule.Length + "\n");
-            // Console.WriteLine("value_num = " + value_num + "!");
-            //
-            // while (i <= rule.Length)
-            // {
-            //     var k = 1 << rule[i - 1];
-            //     Console.WriteLine(i + " -- " + k + " -- " + Convert.ToUInt32(k));
-            //     Console.WriteLine(Convert.ToUInt32(k) & Convert.ToUInt32(value_num));
-            //     res |= Convert.ToUInt32(k) & value_num;
-            //     i++;
-            // }
-            // Console.WriteLine("res = " + res);
+            if (value.Length != rule.Length)
+                throw new ArgumentException("Incorrect value argument");
+            
+            Console.WriteLine("value_num = " + value_num);
+            
+            while (i < rule.Length)
+            {
+                var k = 1 << rule[i] - 1;
+                Console.WriteLine("{0} - {1}", k, value_num);
+                res |= (ulong)k & value_num;
+                i++;
+            }
             return (BitConverter.GetBytes(res));
         }
-
+        
+        static void Main(string[] args)
+        {
+            byte[] mass = new byte[8] {2, 1, 1, 0, 0, 0, 0, 0};
+            for (int i = 0 ; i < mass.Length; i++)
+                Console.Write(mass[i] + " "); 
+            Console.WriteLine();
+            byte[] mass1 = BitSwapping(mass, P_block);
+            for (int i = 0 ; i < mass1.Length; i++)
+                Console.Write(mass1[i] + " ");   
+        }
+        
         // public static byte[] BitReplacement(byte[] mass, int[] S_block, byte k)
         // {
         //     
         //     
         //     return ();
         // }
-
-        static void Main(string[] args)
-        {
-           
-            byte[] mass = new byte[8] {0, 1, 1, 0, 0, 1, 0, 1};
-            
-            byte[] mass1 = new byte[32] {0, 1, 1, 0, 0, 1, 0, 1,
-                0, 1, 1, 0, 0, 1, 0, 1,
-                0, 1, 1, 0, 0, 1, 0, 1,
-                0, 1, 1, 0, 0, 1, 209, 1 };
-
-            Console.WriteLine("Lab 1, problem 1\n");
-            mass = BitSwapping(mass, P_block);
-
-            byte[] arr1 = BitConverter.GetBytes(256);
-
-            byte[] arr0 = { 1, 255, 255, 3};
-            byte[] arr2 = BitConverter.GetBytes(256 * 256 * 256 * 3 + 256 * 256 * 255 + 256 * 255 + 255);
-
-            for (int i = 0; i < arr0.Length; i++)
-                Console.WriteLine(i + " - " + arr0[i]);
-            //for (int i = 0; i < arr1.Length; i++)
-                //Console.WriteLine(i + " - " + arr1[i]);
-            Console.WriteLine();
-            for (int i = 0; i < arr2.Length; i++)
-                Console.WriteLine(i + " - " + arr2[i]);
-            
-        }
+        
     }
 }
