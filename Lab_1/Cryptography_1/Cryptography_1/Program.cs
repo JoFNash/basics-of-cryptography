2d92ce9a2553793b58daf802d9d3b0e9e857d51d
JoFNash <olga@LAPTOP-1H4CR6LI.localdomain>
2022-05-13 18:54:39 +0300

    problem with mass of bytes

--- Lab_1/Cryptography_1/Cryptography_1/Program.cs
+++ Lab_1/Cryptography_1/Cryptography_1/Program.cs
@@ -5,49 +5,66 @@ namespace Cryptography_1
     class Program
     {
         /* P-block */
-        private static int[] P_block = new int[] {16, 7, 20, 21,
-            29, 12, 28, 17,
-            1, 15, 23, 26,
-            5, 18, 31, 10,
-            2, 8, 24, 14,
-            32, 27, 3, 9,
-            19, 13, 30, 6,
-            22, 11, 4, 25};
+        private static int[] P_block = new int[] {7, 1, 4, 8, 3, 6, 5, 2};
 
-        public static byte[] BitSwapping(byte[] mass, int[] rule)
+        public static byte[] BitSwapping(byte[] value, int[] rule)
         {
-            int i;
-            byte[] res = new byte[32];
-            
-            i = 0;
-            while (i < 32)
+            int i = 1;
+            ulong value_num = BitConverter.ToUInt32(value, 0);
+            ulong res = 0;
+
+            if (value.Length != rule.Length)
             {
-                res[i] = mass[P_block[i] - 1];
-                i++;
+                throw new ArgumentException("Incorrect value argument");
             }
-            return (res);
+            
+            // Console.WriteLine("k = " + rule.Length + "\n");
+            // Console.WriteLine("value_num = " + value_num + "!");
+            //
+            // while (i <= rule.Length)
+            // {
+            //     var k = 1 << rule[i - 1];
+            //     Console.WriteLine(i + " -- " + k + " -- " + Convert.ToUInt32(k));
+            //     Console.WriteLine(Convert.ToUInt32(k) & Convert.ToUInt32(value_num));
+            //     res |= Convert.ToUInt32(k) & value_num;
+            //     i++;
+            // }
+            // Console.WriteLine("res = " + res);
+            return (BitConverter.GetBytes(res));
         }
+
+        // public static byte[] BitReplacement(byte[] mass, int[] S_block, byte k)
+        // {
+        //     
+        //     
+        //     return ();
+        // }
 
         static void Main(string[] args)
         {
-            int i;
-            byte[] mass = new byte[32] { 1, 0, 1, 1, 
-                0, 1, 0, 0,
-                1, 1, 1, 1,
-                0, 0, 1, 1,
-                0, 1, 1, 1,
-                1, 1, 0, 1,
-                0, 0, 1, 1,
-                0, 1, 0, 12};
+           
+            byte[] mass = new byte[8] {0, 1, 1, 0, 0, 1, 0, 1};
+            
+            byte[] mass1 = new byte[32] {0, 1, 1, 0, 0, 1, 0, 1,
+                0, 1, 1, 0, 0, 1, 0, 1,
+                0, 1, 1, 0, 0, 1, 0, 1,
+                0, 1, 1, 0, 0, 1, 209, 1 };
 
             Console.WriteLine("Lab 1, problem 1\n");
             mass = BitSwapping(mass, P_block);
-            i = 0;
-            while (i < 32)
-            {
-                Console.WriteLine("{0}, ", mass[i]);
-                i++;
-            }
+
+            byte[] arr1 = BitConverter.GetBytes(256);
+
+            byte[] arr0 = { 1, 255, 255, 3};
+            byte[] arr2 = BitConverter.GetBytes(256 * 256 * 256 * 3 + 256 * 256 * 255 + 256 * 255 + 255);
+
+            for (int i = 0; i < arr0.Length; i++)
+                Console.WriteLine(i + " - " + arr0[i]);
+            //for (int i = 0; i < arr1.Length; i++)
+                //Console.WriteLine(i + " - " + arr1[i]);
+            Console.WriteLine();
+            for (int i = 0; i < arr2.Length; i++)
+                Console.WriteLine(i + " - " + arr2[i]);
             
         }
     }
